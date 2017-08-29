96eb7efe492db3dd9b497a06a77ba4256557be5e
Satan's Son <valyrio.email@gmail.com>
2017-08-29 18:52:27 +0100

    Added while loop

--- nqt.py
+++ nqt.py
@@ -69,7 +69,7 @@ def parse(code):
                 final.append(c)
 
     for char in final:
-        if all(i in '1234567890-.' for i in char):
+        if all(i in '1234567890-.' for i in char) and char not in '-.':
             if '.' in char:
                 parsed.append(float(char))
             else:
@@ -79,24 +79,48 @@ def parse(code):
                 
     return parsed
 
+def lastNindex(string, char, N):
+    if string.count(char) < N:
+        return -1
+    found = 0
+    for i, c in enumerate(string[::-1], 1):
+        if c == char:
+            found += 1
+        if found == N:
+            return len(string) - i
+    return -1
+
+def firstNindex(string, char, N):
+    if string.count(char) < N:
+        return -1
+    found = 0
+    for i, c in enumerate(string):
+        if c == char:
+            found += 1
+        if found == N:
+            return i
+    return -1
+
 def run(program,inputs=[]):
     stack = Stack()
     stack.push(*inputs)
-    if_ = True
     popmath = True
-    for char in parse(program):
-        
-        if not if_:
-            if char == '+':
-                if_ = True
-            continue
+    printed = False
+    loop_depth = 0
+    program = parse(program)
+    index = 0
+    while index < len(program):
+
+        char = program[index]
         
         if isinstance(char, (int,float)):
             stack.push(char)
+            index += 1
             continue
 
         if len(char) > 1:
             stack.push(char)
+            index += 1
             continue
 
         if char in '-*%/^':
@@ -104,6 +128,17 @@ def run(program,inputs=[]):
                 stack.push(eval('stack.pop() {} stack.pop()'.format(char.replace('^','**'))))
             else:
                 stack.push(eval('stack[-1] {} stack[-2]'.format(char.replace('^','**'))))
+
+        if char == '[':
+            loop_depth += 1
+            if not stack[-1]:
+                index = lastNindex(program, ']', loop_depth)
+
+        if char == ']':
+            if stack[-1]:
+                index = firstNindex(program, '[', loop_depth)
+            else:
+                loop_depth -= 1
 
         if char == 's':
             stack.sort()
@@ -135,8 +170,13 @@ def run(program,inputs=[]):
             stack.push(len(stack))
         if char == 'o':
             print(stack.pop())
+            printed = True
+        if char == 'O':
+            print(stack)
+            printed = True
         if char == 'w':
             print(chr(stack.pop()))
+            printed = True
         if char == 'm':
             stack.push(min(stack.pop()))
         if char == 'M':
@@ -147,6 +187,11 @@ def run(program,inputs=[]):
             popmath = False
         if char == '?':
             return
+
+        index += 1
+
+    if printed:
+        return
 
     char = stack.pop()
     if char == 0:
@@ -166,10 +211,8 @@ def run(program,inputs=[]):
             print(char)
             return
     print(char.join(map(chr, map(abs, stack))))
-
-prog = sys.argv[1]
-inputs = map(eval, sys.argv[2:])
-if prog.endswith('.txt'):
-    prog = open(prog).read()
-
-run(prog,inputs)
+    
+if __name__ == '__main__':
+    prog = sys.argv[1]
+    inputs = list(map(eval, sys.stdin.read().splitlines()))
+    run(prog, inputs)
